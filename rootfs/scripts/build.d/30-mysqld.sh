1a9826d1f63e9e0e3ab857c3b355a826e21b9ba6
Ambashta Family <sudhaker.raj@gmail.com>
2021-02-10 18:24:27 -0500

    mysql->mariadb

--- rootfs/scripts/build.d/30-mysqld.sh
+++ rootfs/scripts/build.d/30-mysqld.sh
@@ -3,7 +3,15 @@ apk add --no-cache mariadb-common libaio ncurses-libs libstdc++ pwgen pcre xz-li
 
 apk add --no-cache mariadb mariadb-client
 
-tar cf /tmp/mariadb-micro.tar /usr/bin/mysqld /usr/bin/mysql_install_db /usr/bin/my_print_defaults /usr/share/mariadb/ /usr/bin/mysql /usr/bin/mysqldump /etc/mysql /etc/my.cnf*
+tar cf /tmp/mariadb-micro.tar \
+/usr/bin/mysqld /usr/bin/mariadbd \
+/usr/bin/mysql_install_db /usr/bin/mariadb-install-db \
+/usr/bin/my_print_defaults \
+/usr/share/mariadb/ \
+/usr/lib/mariadb/ \
+/usr/bin/mysql /usr/bin/mariadb \
+/usr/bin/mysqldump /usr/bin/mariadb-dump \
+/etc/mysql /etc/my.cnf*
 
 apk del mariadb mariadb-client
 
