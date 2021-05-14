ec2070b1bbc4e893e0011e586436341bd8ff8c5f
Ambashta Family <sudhaker.raj@gmail.com>
2021-05-14 15:32:36 -0400

    updated s6 version

--- rootfs/scripts/build.d/10-s6-overlay.sh
+++ rootfs/scripts/build.d/10-s6-overlay.sh
@@ -1,5 +1,5 @@
 
 apk add --no-cache curl bash wget ca-certificates
     
-S6_OVERLAY_VERSION=v2.0.0.1
+S6_OVERLAY_VERSION=v2.2.0.3
 curl -sSL https://github.com/just-containers/s6-overlay/releases/download/${S6_OVERLAY_VERSION}/s6-overlay-amd64.tar.gz | tar xfz - -C /
