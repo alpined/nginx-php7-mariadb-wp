2d824a0b50090698d8d4b044e4b71d890f833c14
Ambashta Family <sudhaker.raj@gmail.com>
2025-06-14 22:16:34 -0400

    Update 10-s6-overlay.sh for S6 version

--- rootfs/scripts/build.d/10-s6-overlay.sh
+++ rootfs/scripts/build.d/10-s6-overlay.sh
@@ -1,5 +1,5 @@
-
 apk add --no-cache curl bash wget ca-certificates
-    
-S6_OVERLAY_VERSION=v3.2.1.0
-curl -sSL https://github.com/just-containers/s6-overlay/releases/download/${S6_OVERLAY_VERSION}/s6-overlay-amd64.tar.gz | tar xfz - -C /
+
+S6_OVERLAY_VERSION=3.2.1.0
+curl -sSL https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch.tar.xz | tar -C / -Jxpf -
+curl -sSL https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-i686.tar.xz | tar -C / -Jxpf -
